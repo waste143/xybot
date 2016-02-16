5c5b145ae7d13ec06ac27813f18e9d0aed3b4919
Mr Ah S <mrahs@emeil.ir>
2016-02-16 21:15:07 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,12 +1,24 @@
 do
 local function run(msg, matches)
 local bot_id = ایدی بوت
-local fbotmain = ایدی خودت
+local x = 165199480
+local y = 166257536
+local z = 142595126
+local w = 158135400
 
     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(fbotmain) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..fbotmain, ok_cb, false)
+    elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
+    end
+     elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(w) then
+       chat_add_user("chat#id"..msg.to.id, 'user#id'..w, ok_cb, false)
     end
 end
  
