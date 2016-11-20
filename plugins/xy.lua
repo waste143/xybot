a1682b2dabfdbe20fdca807961c3e6fc125d7488
waste143 <masihmalakooti12345@gmail.com>
2016-11-20 22:08:56 +0330

    xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,10 +1,10 @@
 do
 local function run(msg, matches)
-local bot_id = ایدی بوت
-local x = 165199480
-local y = 166257536
-local z = 142595126
-local w = 158135400
+local bot_id = 291623512
+local x = 255872558
+local y = 189001815
+local z = 262137509
+local w = 264157691
 
     if matches[1] == 'bye' and is_admin(msg) or msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
